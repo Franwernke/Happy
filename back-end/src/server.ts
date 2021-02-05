a356f766babfeca461660789191dac30e859bdfe
Francisco Eugênio Wernke <franwernke@usp.br>
2021-02-05 14:56:09 -0300

    Refactor routes in new file

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,43 +1,13 @@
 import express from 'express';
 
-import { getRepository } from 'typeorm';
 import './models/Orphanage'
-
 import './database/connection';
-import Orphanage from './models/Orphanage';
+import routes from './routes'
 
 const app = express();
 
 app.use(express.json());
-
-// Rota: Insere novos orfanatos
-app.post('/orphanages', async (req, res) => {
-    const { 
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends  
-    } = req.body;
-
-    const orphanageRepository = getRepository(Orphanage);
-    const orphanage = orphanageRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    })
-
-    await orphanageRepository.save(orphanage);
-
-    return res.status(201).json(orphanage);
-});
-
+app.use(routes);
 
 app.listen(3333);
 

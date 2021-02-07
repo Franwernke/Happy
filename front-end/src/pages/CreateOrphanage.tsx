d2d8e3728cbf7eb5998dc7ecbf86ff126478cd9c
Francisco Eugênio Wernke <franwernke@usp.br>
2021-02-07 18:42:00 -0300

    Finish orphanage creation process

--- front-end/src/pages/CreateOrphanage.tsx
+++ front-end/src/pages/CreateOrphanage.tsx
@@ -1,6 +1,7 @@
-import React, { FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
 import { LeafletMouseEvent }from 'leaflet';
+import { useHistory } from "react-router-dom";
 
 import { FiPlus } from "react-icons/fi";
 
@@ -8,13 +9,18 @@ import Sidebar from '../components/Sidebar'
 import mapIcon from '../utils/mapIcon';
 
 import '../styles/pages/create-orphanage.css';
+import api from "../services/api";
 
 export default function CreateOrphanage() {
+  const history = useHistory();
+
   const [name, setName] = useState('');
   const [about, setAbout] = useState('');
   const [instructions, setInstructions] = useState('');
   const [opening_hours, setOpening_Hours] = useState('');
   const [open_on_weekends, setOpenOnWeekends] = useState(true);
+  const [images, setImages] = useState<File[]>([]);
+  const [previewImages, setPreviewImages] = useState<string[]>([]);
 
   
   const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
@@ -33,16 +39,44 @@ export default function CreateOrphanage() {
     return null;
   }
 
-  function handleSubmit (event: FormEvent) {
+  function handleSelectImages (event : ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files) {
+      return;
+    }
+
+    const selectedImages = images === [] ? 
+      Array.from(event.target.files) 
+      : images.concat(Array.from(event.target.files));
+    setImages(selectedImages);
+
+    const selectedImagesPreview = selectedImages.map(image => {
+      return URL.createObjectURL(image);
+    })
+    setPreviewImages(selectedImagesPreview);
+  }
+
+  async function handleSubmit (event: FormEvent) {
     event.preventDefault();
 
-    console.log({
-      position,
-      name,
-      about,
-      instructions,
-      opening_hours
+    const {latitude, longitude} = position;
+
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("about", about);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("instructions", instructions);
+    data.append("opening_hours", opening_hours);
+    data.append("open_on_weekends", String(open_on_weekends));
+    images.forEach(image => {
+      data.append('images', image);
     })
+
+    await api.post('orphanages', data);
+
+    alert('Cadastro realizado com sucesso');
+    history.push('/app')
   }
 
   return (
@@ -97,13 +131,18 @@ export default function CreateOrphanage() {
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image">
-
+              <div className="images-container">
+                {previewImages.map(image => {
+                  return (
+                    <img key={image} src={image} alt={name}/>
+                  )
+                })}
+                <label htmlFor="image[]" className="new-image">
+                  <FiPlus size={24} color="#15b6d6" />
+                </label>
               </div>
-
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
-              </button>
+              <input multiple onChange={handleSelectImages} type="file" id="image[]"/>
+              
             </div>
           </fieldset>
 
@@ -134,15 +173,15 @@ export default function CreateOrphanage() {
               <div className="button-select">
                 <button 
                   type="button" 
-                  onClick={(event) => setOpenOnWeekends(false)} 
-                  className={!open_on_weekends ? 'active' : ''}
+                  onClick={() => setOpenOnWeekends(true)} 
+                  className={open_on_weekends ? 'active' : ''}
                 >
                   Sim
                 </button>
                 <button 
                   type="button" 
-                  onClick={(event) => setOpenOnWeekends(true)} 
-                  className={open_on_weekends ? 'active not' : ''}
+                  onClick={() => setOpenOnWeekends(false)} 
+                  className={!open_on_weekends ? 'active not' : ''}
                   >
                   Não
                 </button>
